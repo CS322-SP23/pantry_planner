ad94b0c3a2252c285504eb0799778f8898f7cc55
Garrettqt <102553912+Garrettqt@users.noreply.github.com>
2023-04-12 22:04:11 +0000

    small changes cleaning up code

--- static/js/main.js
+++ static/js/main.js
@@ -25,3 +25,33 @@ sidebar_settings.addEventListener("click", function() {
 sidebar_shopping_list.addEventListener("click", function() {
   window.location.href = ourURL + "/shopping_list"
 });
+
+function addItemExp() {
+        const itemInput = document.getElementById("item-input");
+        const expInput = document.getElementById("exp-input");
+        const item = itemInput.value;
+        const exp = expInput.value;
+        itemInput.value = "";
+        expInput.value = "";
+        const list = document.getElementById("expList");
+        const listItem = document.createElement("li");
+        const itemHeading = document.createElement("h2");
+        itemHeading.appendChild(document.createTextNode(item));
+        const expPara = document.createElement("p");
+        expPara.appendChild(document.createTextNode(exp));
+        listItem.appendChild(itemHeading);
+        listItem.appendChild(expPara);
+        list.appendChild(listItem);
+      }
+
+function addItemShop() {
+        const itemInput = document.getElementById("shop-input");
+        const item = itemInput.value;
+        itemInput.value = "";
+        const list = document.getElementById("shopList");
+        const listItem = document.createElement("li");
+        const itemHeading = document.createElement("h2");
+        itemHeading.appendChild(document.createTextNode(item));
+        listItem.appendChild(itemHeading);
+        list.appendChild(listItem);
+      }
