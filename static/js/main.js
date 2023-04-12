83186b2259423846a100a8da6ed03c941caf9e00
Garrettqt <102553912+Garrettqt@users.noreply.github.com>
2023-04-12 23:07:55 +0000

    commit message

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,39 @@ var sidebar_recipes = document.getElementById("recipes");
 var sidebar_settings = document.getElementById("settings");
 var sidebar_shopping_list = document.getElementById("shopping_list");
 
+
+import { firebase } from '@firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2DuI-rqRSZYiiEzvBasH4CUeppxX_FoY",
+  authDomain: "pantryplanner-5d480.firebaseapp.com",
+  databaseURL: "https://pantryplanner-5d480-default-rtdb.firebaseio.com",
+  projectId: "pantryplanner-5d480",
+  storageBucket: "pantryplanner-5d480.appspot.com",
+  messagingSenderId: "407621335990",
+  appId: "1:407621335990:web:c6ef5ac9f60b5ba09e9f17",
+  measurementId: "G-56LV409GVC"
+};
+
+firebase.intializeApp(firebaseConfig);
+var foodListDB = firebaseConfig.databaseURL().ref('foodList');
+
+const saveFood = (name, expDate) => {
+  var newFood = foodListDB.push();
+
+  newFood.set({
+      food : food,
+      expDate : expDate,
+  })
+}
+
+
+
+
+
+
+
+
 var ourURL = 'https://airfishi-bug-free-space-lamp-pgwj4rjxwrpf5vq-5000.preview.app.github.dev';
 
 sidebar_pantry_planner.addEventListener("click", function() {
@@ -43,6 +76,7 @@ function addItemExp() {
         listItem.appendChild(expPara);
         list.appendChild(listItem);
 
+        console.log(item, exp);
         saveFood(item, exp);
       }
 
